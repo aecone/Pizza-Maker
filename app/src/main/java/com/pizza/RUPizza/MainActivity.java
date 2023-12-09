45cb7f500ba63cded03d184f7f831850035ff5bb
Andrea Kim <80376288+aecone@users.noreply.github.com>
2023-12-08 21:01:59 -0500

    connected activities

--- app/src/main/java/com/pizza/RUPizza/MainActivity.java
+++ app/src/main/java/com/pizza/RUPizza/MainActivity.java
@@ -1,48 +1,113 @@
 package com.pizza.RUPizza;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import android.os.Bundle;
-
+/**
+ * Demo navigation between 2 activities.
+ * @author Lily Chang
+ */
 public class MainActivity extends AppCompatActivity {
+    private static final int NUMBER = 10;
+    private static String STRING = "CS 213";
 
+    /**
+     * Must implement this callback method to perform any initial set up for the app.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        setupListViews(); //define a helper method to avoid lengthy onCreate().
     }
-//    /**
-//    * A helper method to set up the ListView objects.
-//     * 1. set the adapter
-//     * 2. set the onItemClickListener.
-//     */
-//    private void setupListViews() {
-//        lv1 = findViewById(R.id.lv_1);
-//        lv2 = findViewById(R.id.lv_2);
-//        list1 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, fruits);
-//        list2 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, OS);
-//        lv1.setAdapter(list1);
-//        lv1.setOnItemClickListener(new HandleEvent1());
-//        lv2.setAdapter(list2);
-//        lv2.setOnItemClickListener(new HandleEvent2());
-//    }
-//
-//    /**
-//     * An inner class to handle the event fired by the ListView object lv1.
-//     * Get the adapter first in order to get the selected items.
-//     * Note: use the getApplicationContext() method to get the context object.
-//     */
-//    private class HandleEvent1 implements AdapterView.OnItemClickListener {
-//        @Override
-//        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//            String s = lv1.getAdapter().getItem(position).toString(); //position is the index
-//            Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
-//        }
+
+    /**
+     * A callback method executed right after onCreate().
+     */
+    protected void onStart() {
+        super.onStart();
+        System.out.println("onStart() executed");
+    }
+
+    /**
+     * A callback method executed right after onStart().
+     */
+    protected void onResume() {
+        super.onResume();
+        System.out.println("onResume() executed");
+    }
+
+    /**
+     * The event handler for the button click.
+     * Add extra data to an Intent object and pass the extra data to the Activity being started.
+     * INTKEY is the name to be used to retrieve the extra data NUMBER.
+     * @param view the Android View which fired the event.
+     */
+    public void showInteger(View view) {
+        Intent intent = new Intent(this, BuildOwn.class);
+        intent.putExtra("INTKEY", NUMBER); //the extra data is an integer
+        startActivity(intent);
+    }
+
+    /**
+     * The event handler for the button click.
+     * Add extra data to an Intent object and pass the extra data to the Activity being started.
+     * STRKEY is the name to be used to retrieve the extra data STRING.
+     * @param view the Android View which fired the event.
+     */
+    public void showString(View view) {
+        Intent intent = new Intent(this, BuildOwn.class);
+        intent.putExtra("STRKEY", STRING); //the extra data is a string
+        startActivity(intent);
+    }
+
+    /**
+     * Start an Activity containing a webpage. This will run the browser in order to display the
+     * webpage.
+     * @param view the Android View which fired the event.
+     */
+    public void goRutgers(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://rutgers.edu"));
+        startActivity(intent);
+    }
+
+    /**
+     * Display the current order page when button is clicked
+     * @param view the Android View which fired the event.
+     */
+    public void displayCurrentOrder(View view) {
+        Intent intent = new Intent(this, CurrentOrder.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Display the build own pizza page when button is clicked
+     * @param view the Android View which fired the event.
+     */
+    public void displayBuildOwn(View view) {
+        Intent intent = new Intent(this, BuildOwn.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Display the specialty pizza page when button is clicked
+     * @param view the Android View which fired the event.
+     */
+    public void displaySpecialty(View view) {
+        Intent intent = new Intent(this, Specialty.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Display the store order page when button is clicked
+     * @param view the Android View which fired the event.
+     */
+    public void displayStoreOrder(View view) {
+        Intent intent = new Intent(this, StoreOrder.class);
+        startActivity(intent);
+    }
 }
