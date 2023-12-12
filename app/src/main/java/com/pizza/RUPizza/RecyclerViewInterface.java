0f54df9d6da4f9ec6e52962b3fbe86c9c3836d5b
Eric <58400036+echeung52@users.noreply.github.com>
2023-12-11 23:23:26 -0500

    comments

--- app/src/main/java/com/pizza/RUPizza/RecyclerViewInterface.java
+++ app/src/main/java/com/pizza/RUPizza/RecyclerViewInterface.java
@@ -1,5 +1,13 @@
 package com.pizza.RUPizza;
 
+/**
+ * Interface for the recycler view to handle on click functionality
+ */
 public interface RecyclerViewInterface {
+
+    /**
+     * Method to handle when an item is click and get the position of the item
+     * @param position position of the item clicked
+     */
     public void onItemClick(int position);
 }
